18cfa912783697e5942827142074776ce8b0531e
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2025-12-27 18:00:29 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,5 @@
 import pytest
-
-def get_human_age(cat_age, dog_age):
-    def calculate(age, ratio):
-        if age < 15:
-            return 0
-        if age < 24:
-            return 1
-        return 2 + (age - 24) // ratio
-
-    return [calculate(cat_age, 4), calculate(dog_age, 5)]
+from app.main import get_human_age
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
@@ -21,6 +12,10 @@ def get_human_age(cat_age, dog_age):
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (-1, -1, [0, 0]),
+        (23, 24, [1, 2]),
+        (27, 28, [2, 2]),
+        (28, 29, [3, 3]),
     ]
 )
 def test_get_human_age(cat_age, dog_age, expected):
